1a5879eb381298c87baddaa4948a91237fd4ba6e
Harry-GenAI <s.hareeshchandrakumar@gmail.com>
2026-07-20 21:14:55 +0000

    removing staged files

--- debug_notes.py
+++
@@ -1,8 +0,0 @@
-#1) how to check properly split chunks
-from ingest import load_docs, create_chunks
-
-docs = load_docs()
-chunks = create_chunks(docs)
-
-for i, c in enumerate(chunks):
-    print(f"\n--- Chunk {i} ---\n{c.page_content}")
